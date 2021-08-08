d47bf4b64bf6e2b7b65d38a6b93ef616133361b3
Shawn Yu <shawn.yu5@hotmail.com>
2021-08-08 15:01:29 -0400

    export script updated to have more readiable confirmation messages

--- export.sh
+++ export.sh
@@ -4,15 +4,8 @@
 #---------------------------------
 
 #vim config
-#cp -v config/nvim/init.vim ~/.config/nvim/init.vim
+cp config/nvim/init.vim ~/.config/nvim/init.vim && echo "init.vim exported"
 cp -r config/nvim/settings ~/.config/nvim/ && echo "nvim settings directory exported"
 
 #tmux.conf
 cp  tmux.conf ~/.tmux.conf && echo "tmux config exported"
-
-#read -p "sync bash_alias? (y/n): " ALIAS
-#
-#if [[ ALIAS == 'y' ]]; then
-#	vim -d bash_alias ~/.bash_alias
-#fi
-
