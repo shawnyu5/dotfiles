58c6e9d8b93423c4937787a460389225a6d1e1eb
shawn yu <shawn.yu5@hotmail.com>
2021-08-27 21:55:42 -0400

    cd_with_fzf localized to zshrc
    linux cd goes to `$HOME`, while on windows, it cds to onedrive. So the
    function has been localized to the zshrc

--- windows_config/import.sh
+++ windows_config/import.sh
@@ -3,9 +3,9 @@
 #Date: 2021-06-16
 #---------------------------------
 
-cp -v ~/.bashrc ./bashrc
+cp -v ~/.bashrc ./bashrc && echo "bashrc imported"
 
-cp -v ~/.bash_aliases ./bash_aliases
+cp -v ~/.bash_aliases ./bash_aliases && echo "bash_aliases imported"
 
 
 
