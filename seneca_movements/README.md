a4cdebe87ff39ccf3b7ffcb96e709c0c1f7724ec
Shawn Yu <shawn.yu5@hotmail.com>
2021-09-07 12:54:44 -0400

    linux config updated

---
+++ seneca_movements/README.md
@@ -0,0 +1,4 @@
+# Seneca movements
+
+`function custom_move` - takes directory path as argument 1, and the rest as
+files to be moved. Script passes argument 1 into fzf to determine destination
---
+++ seneca_movements/seneca_movements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#purpose of this file: contains general seneca movement utilities
+#Date: 2021-09-07
+#---------------------------------
+
+# takes directory path as arg 1, the rest are files to be moved
+function custom_move
+{
+    DEST=$(ls -d ${1}/*/ | fzf)
+    # remove directory path from argument list
+    shift 1
+    mv -v "$@" "$DEST"
+    cd "$DEST"
+}
+
+custom_move $@
