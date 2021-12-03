3de15e0277fa5e9438f92fdae1acbf701280ae9b
Shawn Yu <shawn.yu5@hotmail.com>
2021-12-02 22:23:11 -0500

    print debug fixed

--- config/nvim/lua/shawn/print_debug/init.lua
+++ config/nvim/lua/shawn/print_debug/init.lua
@@ -10,10 +10,12 @@ local opts = {
 
 -- wraps message and vaiable in print statment based on file type
 local function wrap_variable(message, variable, ft)
-    for key, value in pairs(opts) do
-        local command = value:gsub("<message>", message)
-        command = command:gsub("<variable>", variable)
-        return command
+    for file_type, print_statement in pairs(opts) do
+        if file_type == ft then
+            local command = print_statement:gsub("<message>", message)
+            command = command:gsub("<variable>", variable)
+            return command
+        end
     end
 end
 
@@ -22,18 +24,16 @@ function M.print_debug()
 
     local message = vim.fn.input("message> ")
 
-    if message == "" then
-        message = nil
-    end
+    -- if message == "" then
+        -- message = nil
+    -- end
 
     local variable = vim.fn.expand("<cword>")
 
     -- wrap variable in print statement with message
     local statement = wrap_variable(message, variable, ft)
-
     vim.fn.append(vim.fn.line('.'), statement)
     vim.cmd("norm! =1j")
-
 end
 
 -- M.print_debug()
