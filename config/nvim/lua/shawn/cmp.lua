9240fef1553cbdb61a00b53b33f72b1cdb3b81e6
Shawn Yu <shawn.yu5@hotmail.com>
2021-12-29 15:55:18 -0500

    got rid of super tab
    Kinda annoying. Completion menu is always there, so going to the next
    snippet location means pretting space and back again to delete the extra
    space

--- config/nvim/lua/shawn/cmp.lua
+++ config/nvim/lua/shawn/cmp.lua
@@ -1,12 +1,7 @@
-local source = require "cmp_buffer.source"
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
---
 -- local lspkind = require "lspkind"
 -- lspkind.init()
 
@@ -18,56 +13,21 @@ cmp.setup {
     },
     mapping = {
         -- ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<Tab>"] = cmp.mapping({
-            c = function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-                else
-                    fallback()
-                end
-            end,
-            i = function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-                elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                    vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
-                else
-                    fallback()
-                end
-            end,
-            s = function(fallback)
-                if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                    vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
-                else
-                    fallback()
-                end
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
             end
-        }),
-        ["<S-Tab>"] = cmp.mapping({
-            c = function()
-                if cmp.visible() then
-                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-                else
-                    cmp.complete()
-                end
-            end,
-            i = function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-                elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                    return vim.api.nvim_feedkeys( t("<Plug>(ultisnips_jump_backward)"), 'm', true)
-                else
-                    fallback()
-                end
-            end,
-            s = function(fallback)
-                if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                    return vim.api.nvim_feedkeys( t("<Plug>(ultisnips_jump_backward)"), 'm', true)
-                else
-                    fallback()
-                end
+        end, { "i", "s",}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
             end
-        }),
+        end, { "i", "s"}
+        ),
     },
     sources = {
         { name = 'nvim_lsp' },
@@ -85,7 +45,6 @@ cmp.setup {
             }
         },
     },
-
     formatting = {
         format = function(entry, vim_item)
             vim_item.menu = ({
--- config/nvim/lua/shawn/ultisnips.lua
+++ config/nvim/lua/shawn/ultisnips.lua
@@ -1,7 +1,7 @@
 print("HEYYY")
 -- vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
 vim.g.UltiSnipsExpandTrigger = '<C-s>'
-vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
+vim.g.UltiSnipsJumpForwardTrigger = '<C-s>'
 vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
 vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
 -- vim.g.UltiSnipsRemoveSelectModeMappings = 0
