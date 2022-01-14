58f9bd906d40c80cadd7cde750775c2a55a393e9
Shawn Yu <shawn.yu5@hotmail.com>
2022-01-14 14:53:42 -0500

    trying out neovim-session-manager

--- config/nvim/lua/shawn/telescope.lua
+++ config/nvim/lua/shawn/telescope.lua
@@ -17,11 +17,26 @@ vim.cmd([[
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 " nnoremap <leader>fe <cmd>lua require 'telescope'.extensions.file_browser.file_browser({ layout_strategy='vertical', layout_config={width=0.8} })<CR>
 nnoremap <leader>fe <cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>
+nnoremap <leader>fw <cmd>Telescope live_grep<cr>
 nnoremap <leader>fc <cmd>lua require('telescope.builtin').find_files({cwd = "~/.config/nvim/"})<CR>
 nnoremap <leader>fr <cmd>Telescope resume<CR>
 command! Config :lua require('telescope.builtin').find_files({cwd = "~/.config/nvim/"})
 
 command! Planets :lua require("telescope.builtin").planets({show_pluto = true})
 ]])
+
+
+require("telescope").setup {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      }
+    }
+  }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require("telescope").load_extension("ui-select")
 
 return M
