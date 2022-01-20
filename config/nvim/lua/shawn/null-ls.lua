c9024dd1861c29abd4e502d9f257d9bed17a28c9
Shawn Yu <shawn.yu5@hotmail.com>
2022-01-20 17:56:29 -0500

    null-ls prettier formatting fix

--- config/nvim/lua/shawn/null-ls.lua
+++ config/nvim/lua/shawn/null-ls.lua
@@ -1,19 +1,22 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-   vim.notify("null-ls not installed...")
-   return
+	vim.notify("null-ls not installed...")
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-   debug = false,
-   sources = {
-      formatting.stylua,
-      formatting.prettier.with({ extra_args = {"--double-quotes", "--tab-width 3" } }),
-      formatting.black.with({ extra_args = { "--fast" }}),
-      -- formatting.clang_format.with({ extra_args = { [[-style="{IndentWidth: 3,TabWidth: 3}"]] }})
-      formatting.clang_format,
-      formatting.markdownlint.lua
-   }
+	debug = true,
+	sources = {
+		formatting.stylua,
+		formatting.prettier.with({ args = {
+			"--tab-width 3",
+			"--double-quotes",
+		} }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.clang_format.with({ extra_args = { [[-style="{IndentWidth: 3,TabWidth: 3}"]] }})
+		formatting.clang_format,
+		formatting.markdownlint.lua,
+	},
 })
