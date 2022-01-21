67a4eb69bb94944b333df07e370189a4c087ae7f
Shawn Yu <55592708+shawnyu5@users.noreply.github.com>
2022-01-21 09:41:57 -0500

    feat: init.vim -> init.lua (#1)
    migration complete

---
+++ config/nvim/init.lua
@@ -0,0 +1,76 @@
+vim.cmd([[
+" placed here for easy access
+set relativenumber
+
+" remaping leader to space
+let mapleader =" "
+]])
+
+-- soueces a vim file
+local function source(file)
+	vim.cmd("source " .. file)
+end
+
+-- luafile ~/.config/nvim/plugin_manager.lua
+require("shawn.plugin_manager")
+source("~/.config/nvim/settings/sets.vim")
+source("~/.config/nvim/settings/maps.vim")
+require("shawn.tokyo-knight")
+source("~/.config/nvim/settings/bash-lanuage-server.vim")
+source("~/.config/nvim/settings/lightLine_settings.vim")
+require("shawn.telescope")
+source("~/.config/nvim/settings/NERDtree_settings.vim")
+require("shawn.autopairs")
+require("shawn.ultisnips")
+require("shawn.harpoon")
+require("shawn.refactor")
+require("shawn.lsp")
+require("shawn.null-ls")
+source("~/.config/nvim/settings/markdown-preview.vim")
+require("shawn.executor_settings")
+source("~/.config/nvim/settings/nerdcommenter_settings.vim")
+require("shawn.session_manager")
+source("~/.config/nvim/settings/vim-you-autocorrect.vim")
+source("~/.config/nvim/settings/vim-closetag.vim")
+require("shawn.cmp")
+require("shawn.lsp_signature")
+source("~/.config/nvim/settings/md_paste.vim")
+
+vim.cmd([[
+" set text width
+autocmd bufreadpre *.md,*.txt setlocal textwidth=80
+
+" disable indentLine plugin in json and markdown files
+" let g:indentLine_fileTypeExclude = ['markdown', 'text', 'help']
+
+" auto enable spellcheck in markdown files
+autocmd BufRead *.md setlocal spell
+
+" autocmd BufRead *.hbs echo "hello"
+
+" aug i3config_ft_detection
+" au!
+" au BufNewFile,BufRead ~/.config/i3/config set filetype=i3config
+" aug end
+
+" autosave a view upon leaving buffer and auto load view upon entering
+" augroup AutoSaveFolds
+" "autocmd!
+" "autocmd BufWinLeave * mkview
+" "autocmd BufWinEnter * silent! loadview
+" augroup END
+
+" trailing white space setting
+" =============================
+" allow no trailing white space
+let b:wstrip_trailing_max = 0
+
+" enable stripping white space when writting to buffer
+let g:wstrip_auto = 1
+
+let g:wstrip_highlight = 0
+
+" markdown table of contents setting
+" ===================================
+let g:vmt_auto_update_on_save = 1
+]])
