6aa1575c475f3bb0ec9d9b9d5bc5f4b1aed9e8f0
shawn yu <shawn.yu5@hotmail.com>
2022-03-31 09:31:48 -0400

    feat(neovim): vim test updates

--- config/nvim/lua/shawn/vim-test.lua
+++ config/nvim/lua/shawn/vim-test.lua
@@ -1,4 +1,30 @@
 local opts = { noremap = true }
+local M = {}
+
+local testCommands = {
+	TestNearest = ":TestNearest",
+	TestFile = ":TestFile",
+	TestSuite = ":TestSuite",
+	TestLast = ":TestLast",
+	TestVisit = ":TestVisit",
+}
+
+M.selectTest = function()
+	vim.ui.select(
+		{ "TestNearest", "TestFile", "TestSuite", "TestLast", "TestVisit" },
+		{ prompt = "Select testing options:" },
+		function(selected)
+			for key, command in pairs(testCommands) do
+				if selected == key then
+					vim.api.nvim_command(command)
+				end
+			end
+		end
+	)
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tt", ":lua require('shawn.vim-test').selectTest()<CR>", opts)
+
 vim.api.nvim_set_keymap("n", "<leader>tn", ":TestNearest<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>tf", ":TestFile<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>ts", ":TestSuite<CR>", opts)
@@ -6,3 +32,5 @@ vim.api.nvim_set_keymap("n", "<leader>ls", ":TestLast<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>tv", ":TestVisit<CR>", opts)
 
 vim.api.nvim_exec("let test#strategy = 'neovim'", true)
+
+return M
