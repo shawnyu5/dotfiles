5df0faeaebdf4a43ab0f8889f58166ecb059ee1e
shawn yu <shawn.yu5@hotmail.com>
2022-04-30 21:37:29 -0400

    feat(neovim): git branch info added to status line

---
+++ .config/nvim/lua/shawn/lightline.lua
@@ -0,0 +1,25 @@
+--- get the current git branch name and return the string. Else returns "-"
+local getGitBranch = function()
+	local f = io.popen("git rev-parse --abbrev-ref HEAD")
+	if f then
+		-- read output of current line
+		local branch = f:read("*l")
+		f:close()
+		if branch then
+			return tostring(branch)
+		else
+			return "-"
+		end
+	end
+end
+
+vim.g.lightline = {
+	colorscheme = "tokyonight",
+	active = {
+		left = { { "mode", "paste" }, { "readonly", "filename", "modified", "gitBranch" } },
+	},
+	component = {
+		gitBranch = getGitBranch(),
+	},
+}
+-- print(" getGitBranch():", vim.inspect(getGitBranch())) -- __AUTO_GENERATED_PRINT_VAR__
