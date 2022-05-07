5cb5ade64d599cd0c1d4da6a69410ca3dc28ffd8
shawn yu <shawn.yu5@hotmail.com>
2022-05-06 20:42:34 -0400

    backed up pacman and aur

--- .config/nvim/lua/shawn/copilot.lua
+++ .config/nvim/lua/shawn/copilot.lua
@@ -1,3 +1,3 @@
--- vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<C-y>")', { expr = true, silent = true })
 -- vim.g.copilot_no_tab_map = true
--- vim.g.copilot_assume_mapped = true
+vim.g.copilot_assume_mapped = true
+-- vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<C-y>")', { expr = true, silent = true })
