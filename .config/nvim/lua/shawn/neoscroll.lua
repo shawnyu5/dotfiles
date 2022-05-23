d295851c5fd10d6301e398996409215c5b5e744e
shawn yu <shawn.yu5@hotmail.com>
2022-05-23 15:14:09 -0400

    feat(neovim): neoscroll updates

--- .config/nvim/lua/shawn/neoscroll.lua
+++ .config/nvim/lua/shawn/neoscroll.lua
@@ -1,13 +1,20 @@
 require("neoscroll").setup({
 	-- All these keys will be mapped to their corresponding default scrolling animation
-	mappings = { "<C-u>", "<C-d>", "<C-y>", "<C-e>" },
-	hide_cursor = true, -- Hide cursor while scrolling
+	mappings = { "<C-u>", "<C-d>" },
+	hide_cursor = false, -- Hide cursor while scrolling
 	stop_eof = false, -- Stop at <EOF> when scrolling downwards
 	use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
 	respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 	cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-	easing_function = nil, -- Default easing function
+	easing_function = "quadratic", -- Default easing function
 	pre_hook = nil, -- Function to run before the scrolling animation starts
 	post_hook = nil, -- Function to run after the scrolling animation ends
 	performance_mode = false, -- Disable "Performance Mode" on all buffers.
 })
+
+local t = {}
+-- Syntax: t[keys] = {function, {function arguments}}
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200" } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200" } }
+
+require("neoscroll.config").set_mappings(t)
