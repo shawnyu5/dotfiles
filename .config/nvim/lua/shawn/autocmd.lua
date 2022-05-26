753a6981e0f7b496aaa36031dd678d8f1d70ce0b
shawn yu <shawn.yu5@hotmail.com>
2022-05-25 21:08:44 -0400

    feat(neovim): added auto organizing imports on save in go files

--- .config/nvim/lua/shawn/autocmd.lua
+++ .config/nvim/lua/shawn/autocmd.lua
@@ -6,12 +6,14 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 
 vim.api.nvim_create_augroup("makeFileIndent", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	group = "makeFileIndent",
 	pattern = "make",
 	command = "setlocal tabstop=4 softtabstop=4 shiftwidth=4",
 })
 
 vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	group = "highlight_yank",
 	pattern = "*",
 	callback = function()
 		require("vim.highlight").on_yank({ timeout = 104 })
@@ -20,8 +22,37 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 
 vim.api.nvim_create_augroup("json_lsp", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	group = "json_lsp",
 	pattern = "tsconfig.json",
 	callback = function()
 		vim.cmd("LspStop")
 	end,
+})
+
+function OrgImports()
+	print("OrgImports")
+	local clients = vim.lsp.buf_get_clients()
+	for _, client in pairs(clients) do
+		local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+		params.context = { only = { "source.organizeImports" } }
+
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 5000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end
+end
+
+-- autocmd BufWritePre *.go lua OrgImports(1000)
+local go_imports = vim.api.nvim_create_augroup("go_imports", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = go_imports,
+	pattern = "*.go",
+	callback = OrgImports,
 })
--- .config/nvim/lua/shawn/telescope.lua
+++ .config/nvim/lua/shawn/telescope.lua
@@ -39,5 +39,6 @@ require("telescope").setup({
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require("telescope").load_extension("ui-select")
+require("telescope").load_extension("file_browser")
 
 return M
