79dbd9a06942b4667f011c746789554b85ed36c9
shawn yu <shawn.yu5@hotmail.com>
2022-06-13 22:21:38 -0400

    feat(neovim): refactoring updates

--- .config/nvim/lua/shawn/refactor.lua
+++ .config/nvim/lua/shawn/refactor.lua
@@ -3,11 +3,26 @@ if not ok then
 	print("refactoring not installed...")
 	return
 end
-refactoring.setup({})
+refactoring.setup({
+	print_var_statements = {
+		-- add a custom print var statement for cpp
+		cpp = {
+			'std::cout << "%s " << %s << "\\n";',
+		},
+	},
+	printf_statements = {
+		-- add a custom printf statement for cpp
+		cpp = {
+			'std::cout << "%s\\n";',
+		},
+	},
+})
 
+-- vim.keymap.set("v", "<leader>rv", require("refactoring").debug.print_var({}), { noremap = true })
 vim.api.nvim_set_keymap("v", "<leader>rv", ":lua require('refactoring').debug.print_var({})<CR>", { noremap = true })
 -- Cleanup function: this remap should be made in normal mode
 vim.api.nvim_set_keymap("n", "<leader>rc", ":lua require('refactoring').debug.cleanup({})<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>rf", ":lua require('refactoring').debug.printf({})<CR>", { noremap = true })
 
 -- load refactoring Telescope extension
 require("telescope").load_extension("refactoring")
