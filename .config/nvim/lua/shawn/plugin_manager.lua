44d0851a4eef560ca7c0fa5983cd887b9ee0fa1d
shawn yu <shawn.yu5@hotmail.com>
2022-09-20 10:04:39 -0400

    feat: deleted packer_compiled

--- .config/nvim/lua/shawn/plugin_manager.lua
+++ .config/nvim/lua/shawn/plugin_manager.lua
@@ -45,60 +45,24 @@ return packer.startup(function(use)
 	use("nvim-lua/popup.nvim")
 
 	use({ "xuhdev/vim-latex-live-preview", ft = { "tex", "plaintex" } })
-	-- use({
-	-- "xiyaowong/nvim-transparent",
-	-- config = function()
-	-- require("transparent").setup({
-	-- enable = true, -- boolean: enable transparent
-	-- extra_groups = { -- table/string: additional groups that should be cleared
-	-- -- In particular, when you set it to 'all', that means all available groups
-
-	-- -- example of akinsho/nvim-bufferline.lua
-	-- "BufferLineTabClose",
-	-- "BufferlineBufferSelected",
-	-- "BufferLineFill",
-	-- "BufferLineBackground",
-	-- "BufferLineSeparator",
-	-- "BufferLineIndicatorSelected",
-	-- "FidgetTitle",
-	-- "FidgetTask",
-	-- "NavicIconsFile",
-	-- "NavicIconsModule",
-	-- "NavicIconsNamespace",
-	-- "NavicIconsPackage",
-	-- "NavicIconsClass",
-	-- "NavicIconsMethod",
-	-- "NavicIconsProperty",
-	-- "NavicIconsField",
-	-- "NavicIconsConstructor",
-	-- "NavicIconsEnum",
-	-- "NavicIconsInterface",
-	-- "NavicIconsFunction",
-	-- "NavicIconsVariable",
-	-- "NavicIconsConstant",
-	-- "NavicIconsString",
-	-- "NavicIconsNumber",
-	-- "NavicIconsBoolean",
-	-- "NavicIconsArray",
-	-- "NavicIconsObject",
-	-- "NavicIconsKey",
-	-- "NavicIconsNull",
-	-- "NavicIconsEnumMember",
-	-- "NavicIconsStruct",
-	-- "NavicIconsEvent",
-	-- "NavicIconsOperator",
-	-- "NavicIconsTypeParameter",
-	-- "NavicText",
-	-- "NavicSeparator",
-	-- },
-	-- exclude = {}, -- table: groups you don't want to clear
-	-- })
-	-- end,
-	-- })
 
 	-- use({ "github/copilot.vim" })
 
-	-- use({ "jinh0/eyeliner.nvim" })
+	-- use({
+	-- "https://github.com/nocksock/do.nvim",
+	-- config = function()
+	-- require("do.nvim").setup({
+	-- -- default options
+	-- message_timeout = 2000, -- how long notifications are shown
+	-- kaomoji_mode = 0, -- 0 kaomoji everywhere, 1 skip kaomoji in doing
+	-- doing_prefix = "Doing: ",
+	-- store = {
+	-- auto_create_file = false, -- automatically create a .do_tasks when calling :Do
+	-- file_name = ".do_tasks",
+	-- },
+	-- })
+	-- end,
+	-- })
 
 	use("wbthomason/packer.nvim")
 	use("nathom/filetype.nvim")
