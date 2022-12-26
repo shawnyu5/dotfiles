6063350530f9559e24f2cceb620c99d2e380919c
shawn yu <shawn.yu5@hotmail.com>
2022-12-26 00:25:48 -0500

    fix(neovim): marksman crashing on entry
    Only way to start marksman is via `:LspStart`. An autocommand fires a
    bit after BufWinEnter to trigger LspStart

--- .config/nvim/lua/shawn/lsp/init.lua
+++ .config/nvim/lua/shawn/lsp/init.lua
@@ -67,6 +67,16 @@ lsp.marksman.setup({
 	end,
 })
 
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	pattern = "*.md",
+	callback = function()
+		vim.defer_fn(function()
+			vim.notify("Restarted marksman lsp...")
+			vim.cmd("LspStart")
+		end, 7000)
+	end,
+})
+
 -- css
 lsp.cssls.setup({
 	on_attach = function(client, bufnr)
