96c44e33eee3e6bc67123c241de86d2cf73dec65
shawn yu <shawn.yu5@hotmail.com>
2023-01-21 15:57:05 -0500

    feat(neovim): add csharp_ls

--- .config/nvim/lua/shawn/lsp/init.lua
+++ .config/nvim/lua/shawn/lsp/init.lua
@@ -78,6 +78,56 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 	once = true,
 })
 
+-- c#
+lsp.csharp_ls.setup({
+	on_attach = function(client, bufnr)
+		utils.on_attach(client, bufnr)
+		utils.format_on_save()
+	end,
+})
+
+-- lsp.omnisharp.setup({
+-- cmd = { "dotnet", "/usr/lib/omnisharp-roslyn/OmniSharp.dll" },
+-- on_attach = function(client, bufnr)
+-- utils.on_attach(client, bufnr)
+-- client.server_capabilities.semanticTokensProvider = nil
+-- end,
+
+-- -- Enables support for reading code style, naming convention and analyzer
+-- -- settings from .editorconfig.
+-- enable_editorconfig_support = true,
+
+-- -- If true, MSBuild project system will only load projects for files that
+-- -- were opened in the editor. This setting is useful for big C# codebases
+-- -- and allows for faster initialization of code navigation features only
+-- -- for projects that are relevant to code that is being edited. With this
+-- -- setting enabled OmniSharp may load fewer projects and may thus display
+-- -- incomplete reference lists for symbols.
+-- enable_ms_build_load_projects_on_demand = false,
+
+-- -- Enables support for roslyn analyzers, code fixes and rulesets.
+-- enable_roslyn_analyzers = false,
+
+-- -- Specifies whether 'using' directives should be grouped and sorted during
+-- -- document formatting.
+-- organize_imports_on_format = false,
+
+-- -- Enables support for showing unimported types and unimported extension
+-- -- methods in completion lists. When committed, the appropriate using
+-- -- directive will be added at the top of the current file. This option can
+-- -- have a negative impact on initial completion responsiveness,
+-- -- particularly for the first few completion sessions after opening a
+-- -- solution.
+-- enable_import_completion = false,
+
+-- -- Specifies whether to include preview versions of the .NET SDK when
+-- -- determining which version to use for project loading.
+-- sdk_include_prereleases = true,
+
+-- -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+-- -- true
+-- analyze_open_documents_only = false,
+-- })
 -- css
 lsp.cssls.setup({
 	on_attach = function(client, bufnr)
