87cbf6a341f078cfedde3a4b6f69bd15baf74305
shawn yu <shawn.yu5@hotmail.com>
2023-02-17 22:57:16 -0500

    refactor(neovim): lsp

---
+++ .config/nvim/lua/shawn/lsp/ts.lua
@@ -0,0 +1,13 @@
+local lsp = require("lspconfig")
+local utils = require("shawn.lsp.utils")
+
+-- tsserver
+lsp.tsserver.setup({
+	on_attach = function(client, bufnr)
+      utils.disable_formatting(client)
+		utils.format_on_save()
+		utils.on_attach(client, bufnr)
+	end,
+})
+
+
---
+++ .config/nvim/lua/shawn/lsp/yaml.lua
@@ -0,0 +1,10 @@
+local lsp = require("lspconfig")
+local utils = require("shawn.lsp.utils")
+
+lsp.yamlls.setup({
+	on_attach = function(client, bufnr)
+		utils.on_attach(client, bufnr)
+		-- utils.format_on_save()
+		require("nvim-navic").attach(client, bufnr)
+	end,
+})
