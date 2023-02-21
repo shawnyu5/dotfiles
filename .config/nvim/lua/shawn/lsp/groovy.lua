0122e3fd70cea5fe39b183375552a11461f8b4e1
shawn yu <shawn.yu5@hotmail.com>
2023-02-21 11:19:59 -0500

    merged neovim config from main

--- .config/nvim/lua/shawn/lsp/groovy.lua
+++ .config/nvim/lua/shawn/lsp/groovy.lua
@@ -1,3 +1,8 @@
-require("lspconfig").groovyls.setup({
+local lsp = require("lspconfig")
+local utils = require("shawn.lsp.utils")
+lsp.groovyls.setup({
 	cmd = { "groovy-language-server" },
+   on_attach = function(client, bufnr)
+      utils.on_attach(client, bufnr)
+   end
 })
