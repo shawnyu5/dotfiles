dc93c40a504ae1b352f8f14ba53e747938fb5d62
shawn yu <shawn.yu5@hotmail.com>
2023-02-26 16:52:36 -0500

    fix(neovim): require commands

--- .config/nvim/lua/shawn/hydra.lua
+++ .config/nvim/lua/shawn/hydra.lua
@@ -1,7 +1,7 @@
 local ok, hydra = pcall(require, "hydra")
 if not ok then
-	vim.notify("hydra not installed...", vim.log.ERROR)
-	return
+   vim.notify("hydra not installed...", vim.log.ERROR)
+   return
 end
 
 local M = {}
@@ -23,68 +23,68 @@ local dap = require("dap")
 local dapui = require("dapui")
 
 M.dap_hydra = hydra({
-	name = "dap",
-	mode = "n",
-	-- body = "<leader>d",
-	heads = {
-		-- {
-		-- "c",
-		-- function()
-		-- dap.continue()
-		-- end,
-		-- { desc = "continue" },
-		-- },
-		{
-			"o",
-			function()
-				dap.step_over()
-			end,
-			{ desc = "Step over" },
-		},
-		{
-			"O",
-			function()
-				dap.step_out()
-			end,
-			{ desc = "Step out" },
-		},
-		{
-			"n",
-			function()
-				dap.step_into()
-			end,
-			{ desc = "Step into" },
-		},
-		{
-			"f",
-			function()
-				dapui.float_element("scopes", { enter = true, width = 75 })
-			end,
-			{ desc = "toggle scopes in floating window" },
-		},
-	},
-	config = {
-		exit = false,
-		-- run all other keys pressed if they are not a hydra head
-		foreign_keys = "run",
-	},
+   name = "dap",
+   mode = "n",
+   -- body = "<leader>d",
+   heads = {
+      -- {
+      -- "c",
+      -- function()
+      -- dap.continue()
+      -- end,
+      -- { desc = "continue" },
+      -- },
+      {
+         "o",
+         function()
+            dap.step_over()
+         end,
+         { desc = "Step over" },
+      },
+      {
+         "O",
+         function()
+            dap.step_out()
+         end,
+         { desc = "Step out" },
+      },
+      {
+         "n",
+         function()
+            dap.step_into()
+         end,
+         { desc = "Step into" },
+      },
+      {
+         "f",
+         function()
+            dapui.float_element("scopes", { enter = true, width = 75 })
+         end,
+         { desc = "toggle scopes in floating window" },
+      },
+   },
+   config = {
+      exit = false,
+      -- run all other keys pressed if they are not a hydra head
+      foreign_keys = "run",
+   },
 })
 
 local take_input = function()
-	vim.ui.select({ "a", "b", "c" }, {
-		prompt = "Input: ",
-		on_select = function(selected)
-			print(selected)
-		end,
-	})
+   vim.ui.select({ "a", "b", "c" }, {
+      prompt = "Input: ",
+      on_select = function(selected)
+         print(selected)
+      end,
+   })
 end
 hydra({
-	name = "input test",
-	mode = "n",
-	body = "t",
-	heads = {
-		{ "s", take_input, { desc = "Take input", exit = true } },
-	},
+   name = "input test",
+   mode = "n",
+   body = "t",
+   heads = {
+      { "s", take_input, { desc = "Take input", exit = true } },
+   },
 })
 
 local hint = [[
@@ -97,49 +97,49 @@ local hint = [[
 ]]
 
 local options_hydra = hydra({
-	name = "Options",
-	hint = hint,
-	config = {
-		color = "amaranth",
-		invoke_on_body = true,
-		hint = {
-			border = "rounded",
-			position = "middle",
-		},
-	},
-	mode = { "n", "x" },
-	-- body = "<leader>o",
-	heads = {
-		{
-			"r",
-			function()
-				if vim.o.relativenumber == true then
-					vim.o.relativenumber = false
-				else
-					vim.o.number = true
-					vim.o.relativenumber = true
-				end
-			end,
-			{ desc = "relativenumber" },
-		},
-		{
-			"s",
-			function()
-				if vim.o.spell == true then
-					vim.o.spell = false
-				else
-					vim.o.spell = true
-				end
-			end,
-			{ exit = true, desc = "spell" },
-		},
-		{ "<Esc>", nil, { exit = true } },
-	},
+   name = "Options",
+   hint = hint,
+   config = {
+      color = "amaranth",
+      invoke_on_body = true,
+      hint = {
+         border = "rounded",
+         position = "middle",
+      },
+   },
+   mode = { "n", "x" },
+   -- body = "<leader>o",
+   heads = {
+      {
+         "r",
+         function()
+            if vim.o.relativenumber == true then
+               vim.o.relativenumber = false
+            else
+               vim.o.number = true
+               vim.o.relativenumber = true
+            end
+         end,
+         { desc = "relativenumber" },
+      },
+      {
+         "s",
+         function()
+            if vim.o.spell == true then
+               vim.o.spell = false
+            else
+               vim.o.spell = true
+            end
+         end,
+         { exit = true, desc = "spell" },
+      },
+      { "<Esc>", nil, { exit = true } },
+   },
 })
 
 -- options hydra to easily toggle common options
 vim.api.nvim_create_user_command("Options", function()
-	options_hydra:activate()
+   options_hydra:activate()
 end, {})
 
 return M
