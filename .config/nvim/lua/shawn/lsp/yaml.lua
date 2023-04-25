88cb1466bd92d99c29ec67eff821df2e1ede8714
shawn yu <shawn.yu5@hotmail.com>
2023-04-25 10:32:47 -0400

    merged neovim config from main

--- .config/nvim/lua/shawn/lsp/yaml.lua
+++ .config/nvim/lua/shawn/lsp/yaml.lua
@@ -4,6 +4,7 @@ local utils = require("shawn.lsp.utils")
 lsp.yamlls.setup({
 	on_attach = function(client, bufnr)
 		utils.on_attach(client, bufnr)
+      vim.lsp.handlers["textDocument/publishDiagnostics"] = nil
 		-- utils.format_on_save()
 		require("nvim-navic").attach(client, bufnr)
 	end,
