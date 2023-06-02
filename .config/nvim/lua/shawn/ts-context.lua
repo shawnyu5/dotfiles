1e3c4d523222f99b1b4aaf1abfe2f40a8924b613
shawn yu <shawn.yu5@hotmail.com>
2023-06-02 09:29:52 -0400

    merged neovim config from main

--- .config/nvim/lua/shawn/ts-context.lua
+++ .config/nvim/lua/shawn/ts-context.lua
@@ -1,4 +1,10 @@
-require("treesitter-context").setup({
+local ok, context = pcall(require, "treesitter-context")
+if not ok then
+   vim.notify("treesitter-context not installed...", vim.log.ERROR)
+   return
+end
+
+context.setup({
 	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 	max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
 	trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
