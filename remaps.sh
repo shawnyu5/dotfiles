d004d8b37a47f070cac7d6dfa20cf2a4e85e6f17
shawn yu <shawn.yu5@hotmail.com>
2023-07-01 19:51:23 -0400

    feat: dont swap escape and capslock
    there is no point

--- remaps.sh
+++ remaps.sh
@@ -1,5 +1,5 @@
 # set key repeat rate. delay 280 milisecond, 40 per sec
 xset r rate 300 45
 # swaping escape and caps lock
-setxkbmap -option caps:swapescape
+# setxkbmap -option caps:swapescape
 echo "remapped"
