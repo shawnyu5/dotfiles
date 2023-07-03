f0d24fa127531ebcf6143cc89d2195ac1208cd0d
shawn yu <shawn.yu5@hotmail.com>
2023-07-03 03:34:59 -0400

    fix: move zsh_tools to the right place

--- linux/export.sh
+++
@@ -1,6 +0,0 @@
-cp ./bashrc ~/.bashrc && echo "bashrc exported"
-cp ./bash_aliases ~/.bash_aliases && echo "bash aliases exported"
-
-cp ./zsh_aliases-custom ~/.zsh_aliases-custom && echo "custom zsh aliases exported"
-cp ./zshrc ~/.zshrc && echo "zshrc exported"
-cp -r ./wallpaper ~/Pictures/wallpaper && echo "wallpapers exported"
