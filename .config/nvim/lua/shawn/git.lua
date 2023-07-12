bf9cebf80245fbfad6d5616a9af44c4a9b781863
shawn yu <shawn.yu5@hotmail.com>
2023-07-12 07:57:03 -0400

    merged config from windows

--- .config/nvim/lua/shawn/git.lua
+++ .config/nvim/lua/shawn/git.lua
@@ -10,8 +10,18 @@ keymap("n", "<leader>gs", function()
             unmerged = "🔄",
             untracked = "📥",
          },
-         layout_strategy = "vertical"
+         layout_strategy = "vertical",
+         initial_mode = "normal"
       })
    end,
    { desc = "Git status" })
 keymap("n", "<leader>gc", function() vim.cmd("Git commit") end, { desc = "Git commit" })
+keymap("n", "<leader>gp", function() vim.cmd("Git push") end, { desc = "Git push" })
+keymap("n", "<leader>gf", function()
+   vim.ui.input({ prompt = "Are you sure you want to force push (y/N)? " }, function(input)
+      if input == "y" then
+         vim.cmd("Git commit --amend --no-edit")
+         vim.cmd("Git push --force")
+      end
+   end)
+end, { desc = "Git force push, prompt for confirmation before force pushing" })
