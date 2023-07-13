ad8ca204d2e0808a9dc741589bfc65d9621023d0
shawn yu <shawn.yu5@hotmail.com>
2023-07-12 22:08:20 -0400

    merged config from main

---
+++ .config/nvim/lua/shawn/nerdcommenter.lua
@@ -0,0 +1,14 @@
+-- Add spaces after comment delimiters by default
+vim.g.NERDSpaceDelims = 1
+
+-- Set a language to use its alternate delimiters by default
+vim.g.NERDAltDelims_python = 1
+
+local augroup = vim.api.nvim_create_augroup("nerdcommenter", {})
+vim.api.nvim_create_autocmd("BufEnter", {
+   group = augroup,
+   pattern = {"*.yml", "*.yaml"},
+   callback = function()
+      vim.g.NERDDefaultAlign = 'left'
+   end
+})
