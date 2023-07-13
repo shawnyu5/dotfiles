902270c7f225dea296def358a53464f5fd75c4e9
shawn yu <shawn.yu5@hotmail.com>
2023-07-12 20:46:19 -0400

    merged config from main

--- .config/nvim/lua/shawn/commands.lua
+++ .config/nvim/lua/shawn/commands.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_user_command("GhOpen", function()
 	local file_name = vim.fn.expand("%")
-	print("function file_name:", vim.inspect(file_name)) -- __AUTO_GENERATED_PRINT_VAR__
-	vim.cmd("!gh browse " .. file_name)
+   local branch_name = vim.fn.system("git rev-parse --abbrev-ref HEAD")
+
+	vim.cmd("!gh browse " .. file_name .. " --branch " .. branch_name)
 end, {})
