bf8ef48e4c9e08e48959b6ef28a06047c9f23759
shawn yu <shawn.yu@hotmail.com>
2023-07-26 09:13:16 -0400

    feat(neovim): use lua for nerdtree config

---
+++ .config/nvim/lua/shawn/nerdtree.lua
@@ -0,0 +1,34 @@
+vim.cmd[[
+nnoremap <leader>nn :NERDTreeToggle<CR>
+nnoremap <leader>nf :NERDTreeFind<CR>
+
+" Start NERDTree when Vim is started without file arguments.
+" autocmd StdinReadPre * let s:std_in=1
+" autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
+
+" Start NERDTree when Vim starts with a directory argument.
+" autocmd StdinReadPre * let s:std_in=1
+" autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
+    " \ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
+
+" Exit Vim if NERDTree is the only window remaining in the only tab.
+autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+
+" Close the tab if NERDTree is the only window remaining in it.
+autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+
+" close tree on open
+let g:NERDTreeQuitOnOpen=1
+" show hidden files by default
+let g:NERDTreeShowHidden=1
+" always delete old buffers when renaming or deleting files
+let g:NERDTreeAutoDeleteBuffer=1
+
+]]
+
+local map = vim.keymap.set
+map("n","<leader>nt", function()
+   vim.cmd("tabe")
+   vim.cmd("NERDTreeToggle | only")
+end, {})
+
