e70284192897e4d54f0ed393bfc959184e55c41e
shawn yu <shawn.yu5@hotmail.com>
2023-08-07 23:16:29 -0400

    remove tmux nerd icon window rename plugin
    The plugin overwrites custom window names. Kinda annoying

--- .config/nvim/lua/shawn/lazy.lua
+++ .config/nvim/lua/shawn/lazy.lua
@@ -306,8 +306,8 @@ local pluginSpec = {
       dependencies = { "nvim-telescope/telescope.nvim" },
    },
    -- {
-   -- "nvim-telescope/telescope-file-browser.nvim",
-   -- dependencies = { "nvim-telescope/telescope.nvim" },
+   --    "nvim-telescope/telescope-file-browser.nvim",
+   --    dependencies = { "nvim-telescope/telescope.nvim" },
    -- },
    { "szw/vim-maximizer" },    -- vim maxmizer,
    { "adelarsq/vim-matchit" }, -- matchit,
