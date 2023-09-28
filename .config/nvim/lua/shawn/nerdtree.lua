f053eccefe6b57800a4588addd03f5b201c893c5
shawn yu <shawn.yu5@hotmail.com>
2023-09-28 13:39:11 -0400

    feat(neovim): resize nerdtree buffer to be width of longest line in buffer

--- .config/nvim/lua/shawn/nerdtree.lua
+++ .config/nvim/lua/shawn/nerdtree.lua
@@ -1,6 +1,4 @@
-vim.cmd[[
-nnoremap <leader>nn :NERDTreeToggle<CR>
-nnoremap <leader>nf :NERDTreeFind<CR>
+vim.cmd([[
 
 " Start NERDTree when Vim is started without file arguments.
 " autocmd StdinReadPre * let s:std_in=1
@@ -24,11 +22,44 @@ let g:NERDTreeShowHidden=1
 " always delete old buffers when renaming or deleting files
 let g:NERDTreeAutoDeleteBuffer=1
 
-]]
+]])
+
+--- Get the length of the longest line in the current buffer.
+local function get_longest_line_length()
+	local longest_length = 0
+	for _, line in ipairs(vim.fn.getline(1, "$")) do
+		local line_length = string.len(line)
+		if line_length > longest_length then
+			longest_length = line_length
+		end
+	end
+	return longest_length
+end
+
+---Resizes the nerdtree buffer. Nerdtree should be open and is the current buffer
+local function resize_nerdtree_buffer()
+	local longest_line = get_longest_line_length()
+	vim.cmd("vertical resize " .. longest_line)
+end
 
 local map = vim.keymap.set
-map("n","<leader>nt", function()
-   vim.cmd("tabe")
-   vim.cmd("NERDTreeToggle | only")
-end, {})
+map("n", "<leader>nt", function()
+	vim.cmd("tabe")
+	vim.cmd("NERDTree | only")
+end, {
+	desc = "Open Nerdtree in a new tab",
+})
 
+map("n", "<leader>nf", function()
+	vim.cmd("NERDTreeFind")
+	resize_nerdtree_buffer()
+end, {
+desc = "Open the current file in Nerdtree"
+})
+
+map("n", "<leader>nn", function()
+	vim.cmd("NERDTreeToggle")
+	resize_nerdtree_buffer()
+end, {
+desc = "Open Nerdtree"
+})
