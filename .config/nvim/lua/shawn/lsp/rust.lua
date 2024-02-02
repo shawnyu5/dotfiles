6cb063ec5b4d6bc2b1b7440abd9ad4a2f3613f00
Shawn Yu <shawn.yu@githubnoreply.com>
2024-02-02 11:15:50 -0500

    chore: sync config from main

--- .config/nvim/lua/shawn/lsp/rust.lua
+++ .config/nvim/lua/shawn/lsp/rust.lua
@@ -1,40 +1,48 @@
-local ok, rt = pcall(require, "rust-tools")
 local utils = require("shawn.lsp.utils")
-if not ok then
-	vim.notify("rust-rools not installed...", vim.log.ERROR)
-	return
-end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
-rt.setup({
-	-- settings = {
-	-- ["rust-analyzer"] = {
-	-- cargo = {
-	-- allFeatures = true,
-	-- },
-	-- },
-	-- },
-	capabilities = capabilities,
+vim.g.rustaceanvim = {
+	-- LSP configuration
 	server = {
 		on_attach = function(client, bufnr)
 			utils.on_attach(client, bufnr)
 			utils.format_on_save(bufnr)
-			rt.inlay_hints.enable()
-
-			-- Hover actions
-			--vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-			-- Code action groups
-			--vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+			vim.keymap.set("n", "<F2>", function()
+				vim.cmd.RustLsp("codeAction")
+			end, { noremap = true, silent = true, buffer = bufnr })
 		end,
 	},
-})
+}
 
--- local lsp = require("lspconfig")
--- -- rust
--- lsp.rust_analyzer.setup({
--- on_attach = function(client, bufnr)
--- utils.on_attach(client, bufnr)
--- utils.format_on_save()
--- end,
+-- rt.setup({
+--    -- settings = {
+--    -- ["rust-analyzer"] = {
+--    -- cargo = {
+--    -- allFeatures = true,
+--    -- },
+--    -- },
+--    -- },
+--    capabilities = capabilities,
+--    server = {
+--       on_attach = function(client, bufnr)
+--          utils.on_attach(client, bufnr)
+--          utils.format_on_save(bufnr)
+--          rt.inlay_hints.enable()
+
+--          -- Hover actions
+--          --vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+--          -- Code action groups
+--          --vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+--       end,
+--    },
 -- })
+
+-- -- local lsp = require("lspconfig")
+-- -- -- rust
+-- -- lsp.rust_analyzer.setup({
+-- -- on_attach = function(client, bufnr)
+-- -- utils.on_attach(client, bufnr)
+-- -- utils.format_on_save()
+-- -- end,
+-- -- })
