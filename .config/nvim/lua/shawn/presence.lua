5a4545259f2be95f894e10843a2aee01ad79b274
Shawn Yu <shawn.yu@githubnoreply.com>
2024-04-09 18:35:43 -0400

    chore: sync config from main

--- .config/nvim/lua/shawn/presence.lua
+++ .config/nvim/lua/shawn/presence.lua
@@ -4,7 +4,7 @@ if not ok then
 	return
 end
 -- The setup config table shows all available config options with their default values:
-require("presence"):setup({
+presence:setup({
 	-- General options
 	auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
 	neovim_image_text = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
