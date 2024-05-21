0123c0697b8b48e148f2e53fb9edf7cd7edd6a55
Shawn Yu <shawn.yu@githubnoreply.com>
2024-05-21 09:10:56 -0400

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/luasnip/init.lua
+++ .config/nvim/lua/shawn/luasnip/init.lua
@@ -1,31 +1,22 @@
-local ok, ls = pcall(require, "luasnip")
-if not ok then
-	print("snip not installed...")
-	return
-end
-local s = ls.snippets
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
+local ls = require("luasnip")
+require("luasnip.loaders.from_snipmate").load()
+require("shawn.luasnip.snippets.go")
+require("shawn.luasnip.snippets.gitcommit")
 
-require("luasnip.loaders.from_snipmate").lazy_load() -- initlize ultisnip style snippet support
+vim.keymap.set({ "i" }, "<C-s>", function()
+	ls.expand()
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+	ls.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+	ls.jump(-1)
+end, { silent = true })
 
-ls.snippets = {
-	all = {
-		s("hi", { t("Wow! Text!") }),
-	},
-}
+-- vim.keymap.set({ "i", "s" }, "<C-E>", function()
+--    if ls.choice_active() then
+--       ls.change_choice(1)
+--    end
+-- end, { silent = true })
+
+-- ls.setup()
