c5e99a3eb990a93d51df9fbf19650ff146f1b508
Shawn Yu <shawn.yu@githubnoreply.com>
2024-11-26 12:21:00 -0500

    chore: sync config from main

--- .config/nvim/lua/shawn/autocmd.lua
+++ .config/nvim/lua/shawn/autocmd.lua
@@ -2,73 +2,73 @@ local create_augroup = vim.api.nvim_create_augroup
 local create_autocmd = vim.api.nvim_create_autocmd
 create_augroup("formatOptions", { clear = true })
 create_autocmd({ "BufEnter" }, {
-   pattern = "*",
-   command = "setlocal formatoptions-=cro",
+	pattern = "*",
+	command = "setlocal formatoptions-=cro",
 })
 
 create_augroup("makeFileIndent", { clear = true })
 create_autocmd({ "BufEnter" }, {
-   group = "makeFileIndent",
-   pattern = "make",
-   command = "setlocal tabstop=4 softtabstop=4 shiftwidth=4",
+	group = "makeFileIndent",
+	pattern = "make",
+	command = "setlocal tabstop=4 softtabstop=4 shiftwidth=4",
 })
 
 create_augroup("highlight_yank", { clear = true })
 create_autocmd({ "TextYankPost" }, {
-   group = "highlight_yank",
-   pattern = "*",
-   callback = function()
-      require("vim.highlight").on_yank({ timeout = 104 })
-   end,
+	group = "highlight_yank",
+	pattern = "*",
+	callback = function()
+		require("vim.hl").on_yank({ timeout = 104 })
+	end,
 })
 
 create_augroup("json_lsp", { clear = true })
 create_autocmd({ "BufEnter" }, {
-   group = "json_lsp",
-   pattern = "tsconfig.json",
-   callback = function()
-      vim.cmd("LspStop")
-   end,
+	group = "json_lsp",
+	pattern = "tsconfig.json",
+	callback = function()
+		vim.cmd("LspStop")
+	end,
 })
 
 function OrgImports()
-   local clients = vim.lsp.buf_get_clients()
-   for _, client in pairs(clients) do
-      local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
-      params.context = { only = { "source.organizeImports" } }
+	local clients = vim.lsp.buf_get_clients()
+	for _, client in pairs(clients) do
+		local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+		params.context = { only = { "source.organizeImports" } }
 
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-      for _, res in pairs(result or {}) do
-         for _, r in pairs(res.result or {}) do
-            if r.edit then
-               vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
-            else
-               vim.lsp.buf.execute_command(r.command)
-            end
-         end
-      end
-   end
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end
 end
 
 local go_imports = create_augroup("go_imports", { clear = true })
 create_autocmd({ "BufWritePre" }, {
-   group = go_imports,
-   pattern = "*.go",
-   callback = OrgImports,
+	group = go_imports,
+	pattern = "*.go",
+	callback = OrgImports,
 })
 
 local termina_auto_group = create_augroup("terminal_auto_group", { clear = true })
 create_autocmd({ "TermOpen" }, {
-   group = termina_auto_group,
-   callback = function()
-      vim.cmd("setlocal nospell")
-   end,
+	group = termina_auto_group,
+	callback = function()
+		vim.cmd("setlocal nospell")
+	end,
 })
 
 vim.filetype.add({
-   extension = {
-      vugu = "html",
-   },
+	extension = {
+		vugu = "html",
+	},
 })
 
 -- local central_ci_augroup = create_augroup("central_ci", {})
@@ -85,9 +85,9 @@ vim.filetype.add({
 
 local terminal_group = create_augroup("terminal", {})
 create_autocmd({ "TermOpen" }, {
-   group = terminal_group,
-   callback = function()
-      vim.opt_local.number = false
-      vim.opt_local.relativenumber = false
-   end
+	group = terminal_group,
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+	end,
 })
