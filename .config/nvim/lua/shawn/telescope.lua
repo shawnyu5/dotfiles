f3b0672d62e7c0e503e69f0beed51027121c6a43
Shawn Yu <shawn.yu@githubnoreply.com>
2024-11-29 19:28:44 -0500

    chore: sync config from main

--- .config/nvim/lua/shawn/telescope.lua
+++ .config/nvim/lua/shawn/telescope.lua
@@ -1,17 +1,18 @@
 local ok, telescope = pcall(require, "telescope")
 if not ok then
-	print("telescope not installed...")
-	return
+   print("telescope not installed...")
+   return
 end
 local M = {}
 
+
 -- use git files if in a git repo, other wise fall back to find_files
 function M.project_files()
-	local opts = {} -- define here if you want to define something
-	local ok = pcall(require("telescope.builtin").git_files, opts)
-	if not ok then
-		require("telescope.builtin").find_files(opts)
-	end
+   local opts = {} -- define here if you want to define something
+   local ok = pcall(require("telescope.builtin").git_files, opts)
+   if not ok then
+      require("telescope.builtin").find_files(opts)
+   end
 end
 
 local keymap = vim.keymap.set
@@ -20,28 +21,24 @@ local keymap = vim.keymap.set
 local telescope_builtin = require("telescope.builtin")
 
 local opts = { noremap = true, silent = true }
-keymap("n", "<leader>fd", function()
-	telescope_builtin.diagnostics()
-end, opts)
+keymap("n", "<leader>fd", function() telescope_builtin.diagnostics() end, opts)
 keymap("n", "<leader>ff", function()
-	telescope_builtin.find_files({
-		hidden = true,
-	})
+   telescope_builtin.find_files({
+      hidden = true
+   })
 end, { desc = "fuzzy find all files in pwd, including hidden files" })
-keymap("n", "<leader>fb", function()
-	telescope_builtin.buffers()
-end, { desc = "fuzzy search all buffers" })
-keymap("n", "<leader>fw", function()
-	telescope_builtin.live_grep({ layout_strategy = "vertical" })
-end, { desc = "fuzzy search words" })
-keymap("n", "<leader>fc", function()
-	telescope_builtin.find_files({ cwd = "~/.config/nvim" })
-end, { desc = "fuzzy search over neovim config" })
+keymap("n", "<leader>fb", function() telescope_builtin.buffers() end, { desc = "fuzzy search all buffers" })
+keymap("n", "<leader>fw", function() telescope_builtin.live_grep({ layout_strategy = "vertical" }) end,
+   { desc = "fuzzy search words" })
+keymap("n", "<leader>fc", function() telescope_builtin.find_files({ cwd = "~/.config/nvim" }) end,
+   { desc = "fuzzy search over neovim config" })
 keymap("n", "<leader>fr", telescope_builtin.resume, { desc = "resume last telescope search" })
 keymap("n", "<leader>fh", telescope_builtin.help_tags, { desc = "fuzzy search vim help tags" })
 keymap({ "n", "v" }, "<leader><leader>fw", function()
-	telescope_builtin.grep_string({ layout_strategy = "vertical" })
+   telescope_builtin.grep_string({ layout_strategy = "vertical" })
 end, { desc = "live grep for word under cursor" })
+
+
 
 vim.cmd([[
    command! Config :lua require('telescope.builtin').find_files({cwd = "~/.config/nvim/"})
@@ -49,27 +46,22 @@ vim.cmd([[
 ]])
 
 require("telescope").setup({
-	pickers = {
-		find_files = {
-			follow = true,
-		},
-	},
-	defaults = {
-		file_ignore_patterns = { ".git/" },
-		path_display = { truncate = 3 },
-		layout_config = {
-			vertical = {
-				preview_cutoff = 20,
-			},
-		},
-	},
-	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown({
-				-- even more opts
-			}),
-		},
-	},
+   defaults = {
+      file_ignore_patterns = { ".git/" },
+      path_display = { truncate = 3 },
+      layout_config = {
+         vertical = {
+            preview_cutoff = 20,
+         },
+      },
+   },
+   extensions = {
+      ["ui-select"] = {
+         require("telescope.themes").get_dropdown({
+            -- even more opts
+         }),
+      },
+   },
 })
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
