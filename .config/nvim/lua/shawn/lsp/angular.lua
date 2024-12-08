4bcd31e0a884bc051e0998e3056c8c04a3ec1e2e
shawn yu <shawn.yu5@hotmail.com>
2024-12-08 09:10:56 -0500

    chore(neovim): remove extra LSP files

--- .config/nvim/lua/shawn/lsp/angular.lua
+++
@@ -1,8 +0,0 @@
-local lsp = require("lspconfig")
-lsp.angularls.setup({
-	on_attach = function(client, bufnr)
-		-- disable rename
-		client.server_capabilities.rename = false
-	end,
-})
-
--- .config/nvim/lua/shawn/lsp/ts.lua
+++
@@ -1,8 +0,0 @@
-local lsp = require("lspconfig")
-local utils = require("shawn.lsp.utils")
-
-lsp.tsserver.setup({
-	on_attach = function(_, bufnr)
-		utils.disable_formatting_on_save(bufnr)
-	end,
-})
