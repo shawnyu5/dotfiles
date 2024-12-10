20753ca161a907aca6a98454c5f312379ecee8d9
Shawn Yu <shawn.yu@githubnoreply.com>
2024-12-10 13:20:59 -0500

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/copilot.lua
+++ .config/nvim/lua/shawn/copilot.lua
@@ -1,21 +1,36 @@
--- vim.g.copilot_no_tab_map = true
--- vim.g.copilot_assume_mapped = true
--- vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<C-y>")', { expr = true, silent = true })
+local copilot = require("copilot")
+copilot.setup({
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		hide_during_completion = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	panel = {
+		enabled = true,
+	},
 
-vim.g.copilot_filetypes = { ["dap-repl"] = false, dapui_watches = false, dapui_scopes = false, TelescopePrompt = false }
-
-local ok, copilot = pcall(require, "copilot")
-if not ok then
-	vim.notify("copilot.lua not installed...", vim.log.ERROR)
-	return
-end
-
-copilot.setup({ ft_disable = { "dap-repl", "dapui_watches", "dapui_scopes", "TelescopePrompt" } })
-
-local ok, copilot_cmp = pcall(require, "copilot_cmp")
-if not ok then
-	vim.notify("copilot_cmp not installed...", vim.log.ERROR)
-	return
-end
-
-copilot_cmp.setup()
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+		["dap-repl"] = false,
+		["dapui_watches"] = false,
+		["dapui_scopes"] = false,
+		["TelescopePrompt"] = false,
+	},
+})
