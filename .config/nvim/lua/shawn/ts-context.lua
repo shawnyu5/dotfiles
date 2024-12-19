06c035cedb75209bb9e2f92bbc00b062d64b4533
Shawn Yu <shawn.yu@githubnoreply.com>
2024-12-18 20:41:54 -0500

    chore: sync config from main

--- .config/nvim/lua/shawn/ts-context.lua
+++ .config/nvim/lua/shawn/ts-context.lua
@@ -6,7 +6,7 @@ end
 
 context.setup({
 	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-	max_lines = 12, -- How many lines the window should span. Values <= 0 mean no limit.
+	max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
 	trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
 	patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
 		-- For all filetypes
