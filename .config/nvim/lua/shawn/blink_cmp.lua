c8aa49aecab1cc66264ff179e78a2f6a1e7b2a44
shawn yu <shawn.yu5@hotmail.com>
2024-12-21 16:19:41 -0500

    feat(neovim): update to blink.cmp 0.8

--- .config/nvim/lua/shawn/blink_cmp.lua
+++ .config/nvim/lua/shawn/blink_cmp.lua
@@ -36,56 +36,12 @@ local config = {
 
 	completion = {
 		list = {
-			-- Maximum number of items to display
-			max_items = 200,
-			-- Controls if completion items will be selected automatically,
-			-- and whether selection automatically inserts
 			selection = "auto_insert",
-			-- Controls how the completion items are selected
-			-- 'preselect' will automatically select the first item in the completion list
-			-- 'manual' will not select any item by default
-			-- 'auto_insert' will not select any item by default, and insert the completion items automatically
-			-- when selecting them
-			--
-			-- You may want to bind a key to the `cancel` command, which will undo the selection
-			-- when using 'auto_insert'
-			cycle = {
-				-- When `true`, calling `select_next` at the *bottom* of the completion list
-				-- will select the *first* completion item.
-				from_bottom = true,
-				-- When `true`, calling `select_prev` at the *top* of the completion list
-				-- will select the *last* completion item.
-				from_top = true,
-			},
 		},
 
-		documentation = {
+		menu = {
 			-- Controls whether the documentation window will automatically show when selecting a completion item
 			auto_show = true,
-			-- Delay before showing the documentation window
-			auto_show_delay_ms = 50,
-			-- Delay before updating the documentation window when selecting a new item,
-			-- while an existing item is still visible
-			update_delay_ms = 50,
-			-- Whether to use treesitter highlighting, disable if you run into performance issues
-			treesitter_highlighting = true,
-			window = {
-				min_width = 10,
-				max_width = 60,
-				max_height = 20,
-				border = "padded",
-				winblend = 0,
-				winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
-				-- Note that the gutter will be disabled when border ~= 'none'
-				scrollbar = true,
-				-- Which directions to show the documentation window,
-				-- for each of the possible menu window directions,
-				-- falling back to the next direction when there's not enough space
-				direction_priority = {
-					menu_north = { "e", "w", "n", "s" },
-					menu_south = { "e", "w", "s", "n" },
-				},
-			},
 		},
 		-- Displays a preview of the selected item on the current line
 		ghost_text = {
@@ -96,34 +52,10 @@ local config = {
 	-- Experimental signature help support
 	signature = {
 		enabled = true,
-		trigger = {
-			blocked_trigger_characters = {},
-			blocked_retrigger_characters = {},
-			-- When true, will show the signature help window when the cursor comes after a trigger character when entering insert mode
-			show_on_insert_on_trigger_character = true,
-		},
-		window = {
-			min_width = 1,
-			max_width = 100,
-			max_height = 20,
-			border = "padded",
-			winblend = 0,
-			winhighlight = "Normal:BlinkCmpSignatureHelp,FloatBorder:BlinkCmpSignatureHelpBorder",
-			scrollbar = false, -- Note that the gutter will be disabled when border ~= 'none'
-			-- Which directions to show the window,
-			-- falling back to the next direction when there's not enough space,
-			-- or another window is in the way
-			direction_priority = { "n", "s" },
-			-- Disable if you run into performance issues
-			treesitter_highlighting = true,
-		},
 	},
 
 	sources = {
-		completion = {
-			-- Static list of providers to enable, or a function to dynamically enable/disable providers based on the context
-			enabled_providers = { "lsp", "luasnip", "snippets", "buffer" },
-		},
+		default = { "lsp", "path", "luasnip", "snippets", "buffer" },
 
 		providers = {
 			-- TODO: this source doesnt seem to work...
@@ -151,7 +83,7 @@ local config = {
 				should_show_items = true, -- Whether or not to show the items
 				max_items = nil, -- Maximum number of items to display in the menu
 				min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
-				fallback_for = {}, -- If any of these providers return 0 items, it will fallback to this provider
+				fallbacks = {},
 				score_offset = 0, -- Boost/penalize the score of the items
 				override = nil, -- Override the source's functions
 			},
