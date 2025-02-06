c3bdc84a0fd489841baf005978cea32efd5c7176
Shawn Yu <shawn.yu@githubnoreply.com>
2025-02-05 22:36:01 -0500

    chore: sync config from main

--- .config/nvim/lua/shawn/lsp/init.lua
+++ .config/nvim/lua/shawn/lsp/init.lua
@@ -47,6 +47,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	group = lspAugroup,
 	callback = function(ev)
 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+		vim.diagnostic.config({ virtual_text = true })
+
 		-- local capabilities = vim.lsp.protocol.make_client_capabilities()
 		-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 		-- capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities())
