65bea5b1beb9cb65598d87357fa6b786cd3a73d6
Shawn Yu <shawn.yu@hotmail.com>
2025-05-28 11:42:26 -0400

    THIS DOESNT WORK AND IDK WHY

--- .config/nvim/lua/shawn/tree-sitter.lua
+++ .config/nvim/lua/shawn/tree-sitter.lua
@@ -1,66 +1,29 @@
 -- Disable Treesitter async parsing to reduce flickering until this issue is fixed: https://github.com/neovim/neovim/pull/33145
-vim.g._ts_force_sync_parsing = true
-require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"javascript",
-		"go",
-		"bash",
-		"lua",
-		"python",
-		"cpp",
-		"java",
-		"typescript",
-		"rust",
-		-- "dockerfile",
-		"markdown",
-		"markdown_inline",
-		"rst",
-		"yaml",
-		"tsx",
-		"c_sharp",
-		"terraform",
-		"json",
-		"toml",
-	},
-	auto_install = true,
-	sync_install = false,
-	highlight = {
-		enable = true,
-		disable = { "text" },
-
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		additional_vim_regex_highlighting = false,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			-- init_selection = "gnn",
-			node_incremental = "<CR>",
-			-- scope_incremental = "grc",
-			node_decremental = "<BS>",
-		},
-	},
-	playground = {
-		enable = true,
-		disable = {},
-		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-		persist_queries = false, -- Whether the query persists across vim sessions
-		keybindings = {
-			toggle_query_editor = "o",
-			toggle_hl_groups = "i",
-			toggle_injected_languages = "t",
-			toggle_anonymous_nodes = "a",
-			toggle_language_display = "I",
-			focus_language = "f",
-			unfocus_language = "F",
-			update = "R",
-			goto_node = "<cr>",
-			show_help = "?",
-		},
-	},
-	query_linter = {
-		enable = true,
-		use_virtual_text = true,
-		lint_events = { "BufWrite", "CursorHold" },
-	},
+-- vim.g._ts_force_sync_parsing = true
+local ts_langs = {
+	"javascript",
+	"go",
+	"bash",
+	"lua",
+	"python",
+	"cpp",
+	"java",
+	"typescript",
+	"rust",
+	"dockerfile",
+	"markdown",
+	"markdown_inline",
+	"rst",
+	"yaml",
+	"tsx",
+	"terraform",
+	"json",
+	"toml",
+}
+require("nvim-treesitter").install(ts_langs)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = ts_langs,
+	callback = function()
+		vim.treesitter.start()
+	end,
 })
