7cc8118298c83e905f1f00d6cc4f8023bcc60606
Shawn Yu <shawn.yu@githubnoreply.com>
2025-08-05 10:34:47 -0400

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/tree-sitter.lua
+++ .config/nvim/lua/shawn/tree-sitter.lua
@@ -1,39 +1,39 @@
 -- Disable Treesitter async parsing to reduce flickering until this issue is fixed: https://github.com/neovim/neovim/pull/33145
 vim.g._ts_force_sync_parsing = true
 local ts_langs = {
-	"javascript",
-	"go",
-	"bash",
-	"lua",
-	"python",
-	"cpp",
-	"java",
-	"typescript",
-	"rust",
-	"dockerfile",
-	"markdown",
-	"markdown_inline",
-	"rst",
-	"yaml",
-	"tsx",
-	"terraform",
-	"json",
-	"toml",
-	"gitcommit",
-	"git_rebase",
-	"gitignore",
-	"git_config",
-	"diff",
-	"graphql",
-	"hcl",
+   "javascript",
+   "go",
+   "bash",
+   "lua",
+   "python",
+   "cpp",
+   "java",
+   "typescript",
+   "rust",
+   "dockerfile",
+   "markdown",
+   "markdown_inline",
+   "rst",
+   "yaml",
+   "tsx",
+   "terraform",
+   "json",
+   "toml",
+   "gitcommit",
+   "git_rebase",
+   "gitignore",
+   "git_config",
+   "diff",
+   "graphql",
+   "hcl",
 }
 require("nvim-treesitter").install(ts_langs)
 -- Register ansible.yaml as a YAML filetype
 vim.treesitter.language.register("yaml", { "ansible.yaml" })
 vim.treesitter.language.register("markdown", { "copilot-chat" })
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = vim.tbl_extend("force", ts_langs, { "ansible.yaml", "copilot-chat" }),
-	callback = function()
-		vim.treesitter.start()
-	end,
+   pattern = vim.list_extend(ts_langs, { "ansible.yaml", "copilot-chat" }),
+   callback = function()
+      vim.treesitter.start()
+   end,
 })
