a1e44df4bfe3e020ec3e08e25a2ffea77f5e1634
shawn yu <shawn.yu5@hotmail.com>
2025-08-07 20:50:48 -0400

    chore(neovim): update plugins

--- .config/nvim/lua/shawn/lazy.lua
+++ .config/nvim/lua/shawn/lazy.lua
@@ -502,6 +502,24 @@ local pluginSpec = {
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
+			-- {
+			--    "dmtrKovalenko/fff.nvim",
+			--    build = "cargo build --release",
+			--    -- or if you are using nixos
+			--    -- build = "nix run .#release",
+			--    opts = {
+			--       -- pass here all the options
+			--    },
+			--    -- keys = {
+			--    --    {
+			--    --       "ff", -- try it if you didn't it is a banger keybinding for a picker
+			--    --       function()
+			--    --          require("fff").find_files() -- or find_in_git_root() if you only want git files
+			--    --       end,
+			--    --       desc = "Open file picker",
+			--    --    },
+			--    -- },
+			-- },
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = function()
