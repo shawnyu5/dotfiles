8e39c2dfdf1baf08da642d9c04d8d17fa8059510
Shawn Yu <shawn.yu@githubnoreply.com>
2025-11-07 10:15:39 -0500

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/copilot_chat.lua
+++ .config/nvim/lua/shawn/copilot_chat.lua
@@ -24,3 +24,11 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>pm", "<Cmd>CopilotChatModels<CR>", {
 	desc = "List available models",
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "copilot-*",
+	callback = function()
+		vim.opt_local.relativenumber = false
+		vim.opt_local.number = false
+	end,
+})
