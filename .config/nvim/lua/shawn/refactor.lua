7107b189c47913ec3272e1f9eafba09f7ddd68fc
shawn yu <shawn.yu5@hotmail.com>
2025-11-07 21:47:39 -0500

    fix(neovim): remove refactoring.nvim telescope dependency

--- .config/nvim/lua/shawn/refactor.lua
+++ .config/nvim/lua/shawn/refactor.lua
@@ -1,9 +1,4 @@
-local ok, refactoring = pcall(require, "refactoring")
-if not ok then
-	vim.notify("refactoring not installed...", vim.log.levels.WARN)
-	return
-end
-
+local refactoring = require("refactoring")
 refactoring.setup({
 	print_var_statements = {
 		-- add a custom print var statement for cpp
@@ -44,13 +39,6 @@ vim.keymap.set(
 vim.api.nvim_set_keymap("n", "<leader>rc", ":lua require('refactoring').debug.cleanup({})<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>rf", ":lua require('refactoring').debug.printf({})<CR>", { noremap = true })
 
--- load refactoring Telescope extension
-require("telescope").load_extension("refactoring")
-
--- open the Telescope refactoring menu in visual mode
-vim.api.nvim_set_keymap(
-	"v",
-	"<leader>rr",
-	"<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
-	{ noremap = true }
-)
+vim.keymap.set({ "n", "x" }, "<leader>rr", function()
+	refactoring.select_refactor()
+end)
