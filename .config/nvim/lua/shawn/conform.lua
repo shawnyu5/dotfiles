e34ee168756e67361dbded5a193ba0c5d8365e0b
Shawn Yu <shawn.yu@githubnoreply.com>
2025-11-30 23:14:05 -0500

    chore: sync config from main

--- .config/nvim/lua/shawn/conform.lua
+++ .config/nvim/lua/shawn/conform.lua
@@ -1,4 +1,5 @@
-require("conform").setup({
+local conform = require("conform")
+conform.setup({
 	format_on_save = function(bufnr)
 		-- Disable with a global or buffer-local variable
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
@@ -19,3 +20,9 @@ require("conform").setup({
 		swift = { "swiftformat" },
 	},
 })
+
+conform.formatters.swiftformat = {
+	append_args = function()
+		return { "--disable", "unusedArguments" }
+	end,
+}
