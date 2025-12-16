954931f64f57d125588fe2f6814a9c6234d9c922
Shawn Yu <shawn.yu@githubnoreply.com>
2025-12-16 12:19:54 -0500

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/lazy.lua
+++ .config/nvim/lua/shawn/lazy.lua
@@ -34,6 +34,22 @@ local pluginSpec = {
 		end,
 	},
 	-- {
+	-- 	"ramilito/kubectl.nvim",
+	-- 	-- use a release tag to download pre-built binaries
+	-- 	version = "2.*",
+	-- 	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	-- 	-- build = 'cargo build --release',
+	-- 	dependencies = "saghen/blink.download",
+	-- 	config = function()
+	-- 		require("kubectl").setup()
+	-- 		vim.api.nvim_create_user_command("K9s", function()
+	-- 			require("kubectl").toggle({})
+	-- 		end, {})
+	-- 	end,
+	-- 	event = "VeryLazy",
+	-- },
+
+	-- {
 	-- 	"shawnyu5/fff.nvim",
 	-- 	branch = "fix/schedule_vim_notify",
 	-- 	build = "cargo build --release",
