f8b7b9ff0b183fec6f51e9ea184f230dddca5cb9
Shawn Yu <shawn.yu@fundserv.com>
2026-02-05 09:52:18 -0500

    chore: update

--- .config/nvim/lua/shawn/lazy.lua
+++ .config/nvim/lua/shawn/lazy.lua
@@ -415,7 +415,12 @@ local pluginSpec = {
 	-- 	dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
 	-- 	version = "^1.0.0",
 	-- },
-	{ "neovim/nvim-lspconfig" },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("shawn.lsp")
+		end,
+	},
 	{ "mfussenegger/nvim-jdtls" },
 	{
 		"mrcjkb/rustaceanvim",
