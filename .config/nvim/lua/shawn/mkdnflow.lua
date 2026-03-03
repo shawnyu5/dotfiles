aefb260228ab1cdca28050e131a64e6932a6f413
Shawn Yu <shawn.yu@githubnoreply.com>
2026-03-03 09:05:50 -0500

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/mkdnflow.lua
+++ .config/nvim/lua/shawn/mkdnflow.lua
@@ -4,4 +4,46 @@ if not ok then
 	return
 end
 
-mkdnflow.setup()
+mkdnflow.setup({
+	mappings = {
+		-- dont make links from text when pressing enter
+		MkdnEnter = false,
+		MkdnTab = false,
+		MkdnSTab = false,
+		-- dont need to jump to next / prev link in file
+		MkdnNextLink = false,
+		MkdnPrevLink = false,
+		MkdnNextHeading = { "n", "]]" },
+		MkdnPrevHeading = { "n", "[[" },
+		-- <C-O> / <C-I> are better for this
+		MkdnGoBack = false,
+		MkdnGoForward = false,
+		MkdnFollowLink = false,
+		MkdnDestroyLink = { "n", "<M-CR>" },
+		MkdnTagSpan = { "v", "<M-CR>" },
+		MkdnMoveSource = false,
+		-- do not override `ya` in normal mode
+		MkdnYankAnchorLink = false,
+		-- dont overide y* mappings
+		MkdnYankFileAnchorLink = false,
+		MkdnIncreaseHeading = { "n", "+" },
+		MkdnDecreaseHeading = false,
+		MkdnToggleToDo = { { "n", "v" }, "<C-Space>" },
+		MkdnNewListItem = false,
+		MkdnNewListItemBelowInsert = { "n", "o" },
+		MkdnNewListItemAboveInsert = { "n", "O" },
+		MkdnExtendList = false,
+		MkdnUpdateNumbering = false,
+		MkdnTableNextCell = false,
+		MkdnTablePrevCell = false,
+		MkdnTableNextRow = false,
+		MkdnTablePrevRow = { "i", "<M-CR>" },
+		MkdnTableNewRowBelow = { "n", "<leader>ir" },
+		MkdnTableNewRowAbove = { "n", "<leader>iR" },
+		MkdnTableNewColAfter = { "n", "<leader>ic" },
+		MkdnTableNewColBefore = { "n", "<leader>iC" },
+		-- these are telescope mappings
+		MkdnFoldSection = false,
+		MkdnUnfoldSection = false,
+	},
+})
