053aaa0cb6b792c0f5c20391ceeed9d2ca891c56
Shawn Yu <shawn.yu@githubnoreply.com>
2026-06-06 20:20:29 -0400

    chore: sync config from main

--- .config/nvim/lua/shawn/obsidian.lua
+++ .config/nvim/lua/shawn/obsidian.lua
@@ -11,4 +11,28 @@ require("obsidian").setup({
 	picker = {
 		name = "snacks.picker",
 	},
+	callbacks = {
+		enter_note = function()
+			local buf = vim.api.nvim_get_current_buf()
+			if vim.b[buf].obsidian_maps_set then
+				return
+			end
+
+			vim.notify("Creating Obsidian notes key maps")
+			-- Get the buffer number of the note we just walked into
+			local buf = vim.api.nvim_get_current_buf()
+			local key_opts = { buffer = buf, remap = false }
+
+			-- key_opts.desc = "Obsidian: Search Titles & Aliases"
+			-- vim.keymap.set("n", "<leader>ff", "<cmd>Obsidian quick_switch<CR>", key_opts)
+
+			key_opts.desc = "Obsidian: Live Grep Text Content"
+			vim.keymap.set("n", "<leader>fg", "<cmd>Obsidian search<CR>", key_opts)
+
+			key_opts.desc = "Obsidian: Open in editor"
+			vim.keymap.set("n", "<leader>m", "<cmd>Obsidian open<CR>", key_opts)
+
+			vim.b[buf].obsidian_maps_set = true
+		end,
+	},
 })
