be47db1df5c7eb8b9660783335af0e71eeee67e7
Shawn Yu <shawn.yu@githubnoreply.com>
2026-06-08 13:54:49 -0400

    chore: sync config from work_laptop

--- .config/nvim/lua/shawn/obsidian.lua
+++ .config/nvim/lua/shawn/obsidian.lua
@@ -30,39 +30,6 @@ local setup = {
 		end,
 	},
 }
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("LspKillMarksmanInObsidianVault", { clear = true }),
-	callback = function(args)
-		local bufnr = args.buf
-		local incoming_client = vim.lsp.get_client_by_id(args.data.client_id)
-		if not incoming_client then
-			return
-		end
-
-		local function logMarksmanShutdown()
-			vim.notify("Marksman LSP shutdown in Obsidian vault")
-		end
-
-		-- 1. Marksman just attached. If Obsidian is already here, kill Marksman immediately.
-		if incoming_client.name == "marksman" then
-			local obsidian_active = #vim.lsp.get_clients({ bufnr = bufnr, name = "obsidian-ls" }) > 0
-			if obsidian_active then
-				vim.lsp.buf_detach_client(bufnr, incoming_client.id)
-				logMarksmanShutdown()
-			end
-
-		-- 2. Obsidian just attached. If Marksman is already running here, terminate it.
-		elseif incoming_client.name == "obsidian-ls" then
-			local active_marksman_clients = vim.lsp.get_clients({ bufnr = bufnr, name = "marksman" })
-			for _, marksman_client in ipairs(active_marksman_clients) do
-				vim.lsp.buf_detach_client(bufnr, marksman_client.id)
-				logMarksmanShutdown()
-			end
-		end
-	end,
-})
-
 if system_config.system_name == "linux" then
 	setup.workspaces = {
 		{
