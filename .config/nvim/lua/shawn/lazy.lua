5f7cb8e2948e4298534f779761b7584dd6ae102b
Shawn Yu <shawn.yu@githubnoreply.com>
2026-08-24 08:55:21 -0400

    chore: sync config from main

--- .config/nvim/lua/shawn/lazy.lua
+++ .config/nvim/lua/shawn/lazy.lua
@@ -62,9 +62,14 @@ local pluginSpec = {
 		-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" }, -- if you use standalone mini plugins
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		event = "VeryLazy",
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
-		opts = {},
+		opts = {
+			presets = "obsidian",
+			-- Render in all modes. This prevents all animation from flickering when entering / exiting insert mode
+			render_modes = true,
+		},
 	},
 	{
 		"MagicDuck/grug-far.nvim",
